8329e116017eb747988e5bc8e834752bb06329c4
paopao74cn <zhangqian06@gmail.com>
2017-11-29 15:19:53 -0600

    update for reproducibility

--- examples/kurator-SPNHC16-YW-xsb/make.sh
+++ examples/kurator-SPNHC16-YW-xsb/make.sh
@@ -12,20 +12,20 @@ mkdir -p $RUN_DIR
 
 # export YW model and extract facts, and draw complete workflow graph with URI template
 $YW_CMD graph
-dot -Tpdf $RESULTS_DIR/complete_wf_graph_uri.gv > $RESULTS_DIR/complete_wf_graph_uri.svg
+dot -Tsvg $RESULTS_DIR/complete_wf_graph_uri.gv > $RESULTS_DIR/complete_wf_graph_uri.svg
 
 # generate subgraphs for subworkflows
 $YW_CMD graph $SCRIPT_DIR/clean_name_date_yw.py -c graph.subworkflow=clean_name_and_date_workflow.clean_scientific_name -c graph.dotfile=$RESULTS_DIR/subgraph_name_val.gv
-dot -Tpdf $RESULTS_DIR/subgraph_name_val.gv > $RESULTS_DIR/subgraph_name_val.svg
+dot -Tsvg $RESULTS_DIR/subgraph_name_val.gv > $RESULTS_DIR/subgraph_name_val.svg
 $YW_CMD graph $SCRIPT_DIR/clean_name_date_yw.py -c graph.subworkflow=clean_name_and_date_workflow.clean_event_date -c graph.dotfile=$RESULTS_DIR/subgraph_date_val.gv
-dot -Tpdf $RESULTS_DIR/subgraph_date_val.gv > $RESULTS_DIR/subgraph_date_val.svg
+dot -Tsvg $RESULTS_DIR/subgraph_date_val.gv > $RESULTS_DIR/subgraph_date_val.svg
 
 # materialize views of YW facts
 $QUERIES_DIR/materialize_yw_views.sh > $VIEWS_DIR/yw_views.P
 
 # draw complete workflow graph (without URI template)
 $QUERIES_DIR/render_complete_wf_graph.sh > $RESULTS_DIR/complete_wf_graph.gv
-dot -Tpdf $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.svg
+dot -Tsvg $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.svg
 
 # list workflow outputs
 $QUERIES_DIR/list_workflow_outputs.sh > $RESULTS_DIR/workflow_outputs.txt
@@ -49,27 +49,27 @@ cp -f $RECON_DIR/reconfacts.P $FACTS_DIR
 
 # draw worfklow graph upstream of data_with_cleaned_names_and_dates
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh data_with_cleaned_names_and_dates > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names_and_dates.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names_and_dates.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names_and_dates.svg
 
 # draw worfklow graph upstream of date_cleaning_log
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'date_cleaning_log\' > $RESULTS_DIR/wf_upstream_of_date_cleaning_log.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_date_cleaning_log.gv > $RESULTS_DIR/wf_upstream_of_date_cleaning_log.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_date_cleaning_log.gv > $RESULTS_DIR/wf_upstream_of_date_cleaning_log.svg
 
 # draw worfklow graph upstream of name_cleaning_log
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'name_cleaning_log\' > $RESULTS_DIR/wf_upstream_of_name_cleaning_log.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_name_cleaning_log.gv > $RESULTS_DIR/wf_upstream_of_name_cleaning_log.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_name_cleaning_log.gv > $RESULTS_DIR/wf_upstream_of_name_cleaning_log.svg
 
 # draw worfklow graph upstream of record_id_data
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'record_id_data\' > $RESULTS_DIR/wf_upstream_of_record_id_data.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_record_id_data.gv > $RESULTS_DIR/wf_upstream_of_record_id_data.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_record_id_data.gv > $RESULTS_DIR/wf_upstream_of_record_id_data.svg
 
 # draw worfklow graph upstream of data_with_cleaned_names
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'data_with_cleaned_names\' > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names.gv > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names.gv > $RESULTS_DIR/wf_upstream_of_data_with_cleaned_names.svg
 
 # draw worfklow graph upstream of original_dataset
 $QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'original_dataset\' > $RESULTS_DIR/wf_upstream_of_original_dataset.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_original_dataset.gv > $RESULTS_DIR/wf_upstream_of_original_dataset.svg
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_original_dataset.gv > $RESULTS_DIR/wf_upstream_of_original_dataset.svg
 
 
 ############## 
@@ -92,19 +92,19 @@ $QUERIES_DIR/list_inputs_upstream_of_data_q2.sh name_cleaning_log name_cleaning_
 
 # draw worfklow graph downstream of original_dataset
 $QUERIES_DIR/render_wf_graph_downstream_of_data_q3.sh \'original_dataset\' > $RESULTS_DIR/wf_downstream_of_original_dataset.gv
-dot -Tpdf $RESULTS_DIR/wf_downstream_of_original_dataset.gv > $RESULTS_DIR/wf_downstream_of_original_dataset.svg
+dot -Tsvg $RESULTS_DIR/wf_downstream_of_original_dataset.gv > $RESULTS_DIR/wf_downstream_of_original_dataset.svg
 
 # draw worfklow graph downstream of local_authority_source
 $QUERIES_DIR/render_wf_graph_downstream_of_data_q3.sh \'local_authority_source\' > $RESULTS_DIR/wf_downstream_of_local_authority_source.gv
-dot -Tpdf $RESULTS_DIR/wf_downstream_of_local_authority_source.gv > $RESULTS_DIR/wf_downstream_of_local_authority_source.svg
+dot -Tsvg $RESULTS_DIR/wf_downstream_of_local_authority_source.gv > $RESULTS_DIR/wf_downstream_of_local_authority_source.svg
 
 # draw worfklow graph downstream of data_with_cleaned_names
 $QUERIES_DIR/render_wf_graph_downstream_of_data_q3.sh data_with_cleaned_names > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names.gv
-dot -Tpdf $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names.gv > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names.svg
+dot -Tsvg $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names.gv > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names.svg
 
 # draw worfklow graph downstream of data_with_cleaned_names_and_dates
 $QUERIES_DIR/render_wf_graph_downstream_of_data_q3.sh data_with_cleaned_names_and_dates > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names_and_dates.gv
-dot -Tpdf $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names_and_dates.svg
+dot -Tsvg $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_downstream_of_data_with_cleaned_names_and_dates.svg
 
 
 ##############
@@ -127,11 +127,11 @@ $QUERIES_DIR/list_outputs_downstream_of_data_q4.sh data_with_cleaned_names data_
 
 # draw recon worfklow graph upstream of data_with_cleaned_names_and_dates
 $QUERIES_DIR/render_wf_recon_graph_upstream_of_data_q5.sh data_with_cleaned_names_and_dates > $RESULTS_DIR/wf_recon_upstream_of_data_with_cleaned_names_and_dates.gv
-dot -Tpdf $RESULTS_DIR/wf_recon_upstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_recon_upstream_of_data_with_cleaned_names_and_dates.svg
+dot -Tsvg $RESULTS_DIR/wf_recon_upstream_of_data_with_cleaned_names_and_dates.gv > $RESULTS_DIR/wf_recon_upstream_of_data_with_cleaned_names_and_dates.svg
 
 # draw recon worfklow graph upstream of name_cleaning_log
 $QUERIES_DIR/render_wf_recon_graph_upstream_of_data_q5.sh name_cleaning_log > $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.gv
-dot -Tpdf $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.gv > $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.svg
+dot -Tsvg $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.gv > $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.svg
 
 
 ##############
@@ -141,4 +141,4 @@ dot -Tpdf $RESULTS_DIR/wf_recon_upstream_of_name_cleaning_log.gv > $RESULTS_DIR/
 
 # draw recon workflow graph with all observables
 $QUERIES_DIR/render_recon_complete_wf_graph_q6.sh > $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv
-dot -Tpdf $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv > $RESULTS_DIR/wf_recon_complete_graph_all_observables.svg
+dot -Tsvg $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv > $RESULTS_DIR/wf_recon_complete_graph_all_observables.svg
